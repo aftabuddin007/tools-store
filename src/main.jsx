ef5132aa5feaf35730bcdec653f13074678bbb03
Md Aftab Uddin <aftabhasan7856@gmail.com>
2025-10-07 20:30:41 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Routes/Root.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
